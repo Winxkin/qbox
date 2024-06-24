b2a0c311a4d70bd302052471848e7082d9c8f737
huannguyen <winxkin@DESKTOP-O1M4101>
2024-06-24 15:41:22 +0700

    update debug json file template

--- script/run_build.sh
+++ script/run_build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+$build_type
 
 # # Configure the build with CMake
 cd ..
-cmake -B build -DCPM_SOURCE_CACHE=$(pwd)/Packages -DLIBQEMU_TARGETS="aarch64;riscv64;riscv32"
+cmake -B build -DCPM_SOURCE_CACHE=$(pwd)/Packages -DLIBQEMU_TARGETS="aarch64;riscv64;riscv32" -DCMAKE_BUILD_TYPE="DEBUG"
 
 # # Change directory to the build directory
 cd build
