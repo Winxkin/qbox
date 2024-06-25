f73ac11434e5acb42c294477d67db75c830853af
huannguyenduy <huannd25@viettel.com.vn>
2024-06-25 15:41:32 +0700

    update dummy register to check pass or fail of application

--- systemc-components/dummy/include/dummy.h
+++ systemc-components/dummy/include/dummy.h
@@ -10,6 +10,7 @@
 
 #include <mutex>
 #include <queue>
+#include <iostream>
 
 #include "tlm.h"
 #include "tlm_utils/simple_target_socket.h"
@@ -35,8 +36,9 @@ public:
     dummy(sc_core::sc_module_name name)
         : socket("target_socket")
     {
-        SCP_TRACE(()) << "dummy constructor";
+        std::cout << "[dummy] constructor\n";
         socket.register_b_transport(this, &dummy::b_transport);
+        this->dummy_reg = 0;
     }
 
     dummy() = delete;
@@ -50,28 +52,54 @@ public:
         uint64_t addr = trans.get_address();
         unsigned int len = trans.get_data_length();
 
+
         trans.set_dmi_allowed(false);
         trans.set_response_status(tlm::TLM_OK_RESPONSE);
 
         switch (trans.get_command()) {
         case tlm::TLM_WRITE_COMMAND:
-            SCP_TRACE(()) << "dummy TLM_WRITE_COMMAND";
-            SCP_TRACE(()) << "-----------------------";
-            SCP_TRACE(()) << "      TM is Pass       ";
-            SCP_TRACE(()) << "-----------------------";
-            exit(0);
+        {
+            std::cout << "[dummy] TLM_WRITE_COMMAND\n";
+            uint32_t reg_temp =  (ptr[3] << 24) | (ptr[2] << 16) | (ptr[1] << 8) | ptr[0];
+            this->update_reg(addr, reg_temp);
+            this->check_dummy_reg();
             break;
+        }
         case tlm::TLM_READ_COMMAND:
-            SCP_TRACE(()) << "dummy TLM_READ_COMMAND";
-            SCP_TRACE(()) << "-----------------------";
-            SCP_TRACE(()) << "      TM is Fail       ";
-            SCP_TRACE(()) << "-----------------------";
-            exit(0);
+        {
+            std::cout << "[dummy] TLM_READ_COMMAND\n";
             break;
+        }
         default:
             break;
         }
     };
+private:
+    void update_reg(uint64_t addr, uint32_t value)
+    {
+        this->dummy_reg = value;
+        std::cout << "[dummy] [update_reg] dummy_reg: " << dummy_reg << "\n";
+    };
+
+    void check_dummy_reg()
+    {
+        if(dummy_reg == 0)
+        {
+            std::cout << "-----------------------\n";
+            std::cout << "      TM is Pass       \n";
+            std::cout << "-----------------------\n";
+            exit(0);
+        }
+        else
+        {
+            std::cout << "-----------------------\n";
+            std::cout << "      TM is Fail       \n";
+            std::cout << "-----------------------\n";
+            exit(0);
+        }
+    };
+
+    uint32_t dummy_reg;
 
 };
 
